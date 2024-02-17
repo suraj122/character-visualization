b65fa293e4b6745eefd74ae6a1cd724620f89b44
Mukesh kumar <122suraj0@gmail.com>
2024-02-17 20:58:24 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-xl font-bold text-center">
+        Welcome to Character Visualization!
+      </h1>
+    </>
+  );
+}
+
+export default App;
