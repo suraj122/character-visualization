ed6b1d86ab70b186879b49b13c9fa7a85577e700
Mukesh kumar <122suraj0@gmail.com>
2024-02-18 00:43:40 +0530

    add pagination to show the character of next and prev page

--- src/components/AllCharacter.jsx
+++ src/components/AllCharacter.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import useAllCharacter from "../hooks/useAllCharacter";
 import CharacterCard from "./CharacterCard";
 import { Link } from "react-router-dom";
 import Shimmer from "./Shimmer";
 
 const AllCharacter = () => {
-  const characters = useAllCharacter();
+  const [pageNum, setPageNum] = useState(40);
+  const characters = useAllCharacter(pageNum);
+
+  const handleNext = () => {
+    if (pageNum < 42) setPageNum(pageNum + 1);
+  };
+  const handlePrev = () => {
+    if (pageNum > 1) setPageNum(pageNum - 1);
+  };
 
   if (characters === null) return <Shimmer />;
   return (
@@ -18,6 +26,28 @@ const AllCharacter = () => {
             </Link>
           ))}
         </div>
+        <footer className="flex justify-center items-center mt-12 gap-4">
+          {pageNum === 1 ? (
+            ""
+          ) : (
+            <button
+              onClick={handlePrev}
+              className="bg-orange-600 text-white font-semibold rounded px-4 py-2"
+            >
+              Prev
+            </button>
+          )}
+          {pageNum === 42 ? (
+            ""
+          ) : (
+            <button
+              onClick={handleNext}
+              className="bg-orange-600 text-white font-semibold rounded px-4 py-2"
+            >
+              Next
+            </button>
+          )}
+        </footer>
       </div>
     </section>
   );
--- src/hooks/useAllCharacter.js
+++ src/hooks/useAllCharacter.js
@@ -1,15 +1,14 @@
 import { useEffect, useState } from "react";
 import { API_URL } from "../utils/constant";
 
-const useAllCharacter = () => {
+const useAllCharacter = (pageNum) => {
   const [allCharacterData, setAllCharacterData] = useState(null);
-
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [pageNum]);
 
   const fetchData = async () => {
-    const data = await fetch(API_URL);
+    const data = await fetch(API_URL + "/?page=" + pageNum);
     const json = await data.json();
     setAllCharacterData(json.results);
   };
